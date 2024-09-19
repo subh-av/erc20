621712b229eb9dce5a3ad4b461f2c4f32b7883c3
subh-av <subhavprakash2003@gmail.com>
2024-09-20 01:38:07 +0530

    final

--- contracts/subhav.sol
+++ contracts/subhav.sol
@@ -1,72 +1,99 @@
-// SPDX-License-Identifier: MIT 
-pragma solidity >=0.7.0 <0.9.0; 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
-//ERC Token Standard #20 Interface
- 
+// ERC Token Standard #20 Interface
+
 interface ERC20Interface {
     function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view  returns (uint balance);
-    function allowance(address tokenOwner, address spender) external view  returns (uint remaining);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
     function transfer(address to, uint tokens) external returns (bool success);
     function approve(address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
- 
+
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
- 
-//Actual token contract
- 
-contract subhav is ERC20Interface {
+
+// Actual token contract
+
+contract Subhav is ERC20Interface {
     string public symbol;
-    string public  name;
+    string public name;
     uint8 public decimals;
     uint public _totalSupply;
- 
+
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
- 
-    constructor(){
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can execute this");
+        _;
+    }
+
+    constructor() {
         symbol = "SUB";
         name = "SUBHAV Koin";
         decimals = 18;
         _totalSupply = 1_000_001_000_000_000_000_000_000;
-        balances[0x5173b4c81336b233278CDABdA7a389D57a4c114a] = _totalSupply;
-        emit Transfer(address(0), 0x5173b4c81336b233278CDABdA7a389D57a4c114a, _totalSupply);
+        balances[0xB66EeDf3c09d8E868CC8a6eD049441093013cE67] = _totalSupply;
+        emit Transfer(address(0), 0xB66EeDf3c09d8E868CC8a6eD049441093013cE67, _totalSupply);
+        owner = msg.sender;
     }
- 
+
     function totalSupply() public view returns (uint) {
-        return _totalSupply  - balances[address(0)];
+        return _totalSupply - balances[address(0)];
     }
- 
+
     function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
- 
+
     function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender]- tokens;
-        balances[to] = balances[to]+tokens;
+        require(balances[msg.sender] >= tokens, "Insufficient balance");
+        balances[msg.sender] -= tokens;
+        balances[to] += tokens;
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
- 
+
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
- 
+
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = (balances[from]-tokens);
-        allowed[from][msg.sender] = (allowed[from][msg.sender]- tokens);
-        balances[to] = (balances[to]+ tokens);
+        require(balances[from] >= tokens, "Insufficient balance");
+        require(allowed[from][msg.sender] >= tokens, "Allowance exceeded");
+
+        balances[from] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
         emit Transfer(from, to, tokens);
         return true;
     }
- 
+
     function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
- 
+
+    // Mint new tokens
+    function mint(address to, uint tokens) public onlyOwner returns (bool success) {
+        _totalSupply += tokens;
+        balances[to] += tokens;
+        emit Transfer(address(0), to, tokens);
+        return true;
+    }
+
+    // Burn tokens
+    function burn(uint tokens) public returns (bool success) {
+        require(balances[msg.sender] >= tokens, "Insufficient balance to burn");
+        balances[msg.sender] -= tokens;
+        _totalSupply -= tokens;
+        emit Transfer(msg.sender, address(0), tokens);
+        return true;
+    }
 }
